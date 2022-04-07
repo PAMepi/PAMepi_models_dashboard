a574777f8101f84876c2458bcf6e7d32cd237f50
Gabriela Borges <gabriela.borges@neoway.com.br>
2022-04-07 18:56:16 -0300

    removes useless components

--- frontend-dashboard/src/app/app.module.ts
+++ frontend-dashboard/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ChartRtComponent } from './components/chart-rt/chart-rt.component';
-import { ChartTxComponent } from './components/chart-tx/chart-tx.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // MATERIAL MODULES
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -17,8 +15,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
-    ChartRtComponent,
-    ChartTxComponent
   ],
   imports: [
     BrowserModule,
