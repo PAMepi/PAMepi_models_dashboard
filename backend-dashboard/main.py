4b6f4f6d1197afd10c057e77e67f34ee964ceb06
Glenda <glenda.lborges2@gmail.com>
2022-04-11 17:12:45 -0300

    new route test

--- backend-dashboard/main.py
+++ backend-dashboard/main.py
@@ -21,6 +21,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 @app.get("/api")
 async def root():
     return {"message": "Hello World"}
