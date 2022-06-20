cd3a2672a970352ae576b6aa72c94d0d4b9c07a4
Gabriela Borges <gabriela.borges@neoway.com.br>
2022-06-20 11:53:50 -0500

    Add locationstrategy on app.module.ts

--- frontend-dashboard/src/app/app-routing.module.ts
+++ frontend-dashboard/src/app/app-routing.module.ts
@@ -3,20 +3,19 @@ import { ChartMapsComponent } from './components/chart-maps/chart-maps.component
 import { ChartModelsComponent } from './components/chart-models/chart-models.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/chart', pathMatch:'full'},
+  { path: '', redirectTo: '/chart', pathMatch: 'full' },
   { path: 'chart', component: ChartModelsComponent },
-  {path: 'map', component:ChartMapsComponent},
-  {path: 'models', component:ModelComparisonComponent}
+  { path: 'map', component: ChartMapsComponent },
+  { path: 'models', component: ModelComparisonComponent }
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-   declarations: [
+  declarations: [
 
   ]
 })
